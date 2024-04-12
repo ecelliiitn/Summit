74571c519b2e35e96e5bfb58549f984c43549e6d
Sarvesh Bajaj <bt22cse087@iiitn.ac.in>
2024-04-12 14:04:15 +0530

    third commit first phase

--- script.js
+++ script.js
@@ -2,42 +2,42 @@ const menubtn = document.querySelector(".nav-menu-btn");
 const closebtn = document.querySelector(".nav-close-btn");
 const navigation = document.querySelector(".navigation");
 
-menubtn.addEventListener("click",()=>{
-    navigation.classList.add("active");
+menubtn.addEventListener("click", () => {
+  navigation.classList.add("active");
 });
 
-closebtn.addEventListener("click",()=>{
-    navigation.classList.remove("active");
+closebtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
 });
 
-const text =document.querySelector(".text-content");
-window.addEventListener("load",()=>{
-    text.classList.add("text-active");
+const text = document.querySelector(".text-content");
+window.addEventListener("load", () => {
+  text.classList.add("text-active");
 });
 
-setTimeout(function() {
-    document.querySelector('.loading-animation').style.display = 'none';
-    
-  }, 4000);
+setTimeout(function () {
+  document.querySelector('.loading-animation').style.display = 'none';
+
+}, 4000);
 
 
 
-  document.querySelectorAll('.nav-items a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('.nav-items a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
 
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        }
-    });
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
 });
-  
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const timeline = document.querySelector('.timeline');
@@ -46,10 +46,10 @@ document.addEventListener('DOMContentLoaded', () => {
     entries.forEach(entry => {
       if (entry.intersectionRatio > 0) {
         const timelineLine = timeline.classList.add('animate-timeline');
-         // Add class to trigger animation
-         containers.forEach((container)=>{
-           const containerl = container.classList.add('animate-container');
-         })
+        // Add class to trigger animation
+        containers.forEach((container) => {
+          const containerl = container.classList.add('animate-container');
+        })
       }
     });
   });
@@ -58,14 +58,14 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   const speakers = document.querySelectorAll(".speaker");
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.intersectionRatio > 0) {
-         
-          const containero = entry.target.classList.add('animate-speaker');
-       
+
+        const containero = entry.target.classList.add('animate-speaker');
+
       }
     });
   });
@@ -78,4 +78,63 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-  
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+  constructor(container, items, controls) {
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
+  }
+
+  updateGallery() {
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1');
+      el.classList.remove('gallery-item-2');
+      el.classList.remove('gallery-item-3');
+      el.classList.remove('gallery-item-4');
+      el.classList.remove('gallery-item-5');
+    })
+
+    this.carouselArray.slice(0, 5).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i + 1}`);
+    })
+  }
+
+  setCurrentState(direction) {
+    if (direction.className == 'gallery-controls-previous') {
+      this.carouselArray.unshift(this.carouselArray.pop());
+    } else {
+      this.carouselArray.push(this.carouselArray.shift());
+    }
+    this.updateGallery();
+  }
+
+  setControls() {
+    this.carouselControls.forEach(control => {
+      const button = document.createElement('button');
+      button.className = `gallery-controls-${control}`;
+      button.innerText = control;
+      galleryControlsContainer.appendChild(button);
+    });
+  }
+
+  useControls() {
+    this.carouselControls.forEach(control => {
+      const button = document.querySelector(`.gallery-controls-${control}`);
+      button.addEventListener('click', e => {
+        e.preventDefault();
+        this.setCurrentState(button);
+      });
+    });
+  }
+
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
