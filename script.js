ad57fc98390ee45e1a05ddff2257e188ad039f79
Sarvesh Bajaj <bt22cse087@iiitn.ac.in>
2024-04-12 19:16:26 +0530

    save-content

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ setTimeout(function () {
 
 document.querySelectorAll('.nav-items a').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+    //e.preventDefault();
 
     const targetId = this.getAttribute('href').substring(1);
     const targetElement = document.getElementById(targetId);
